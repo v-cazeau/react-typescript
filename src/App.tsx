6ea5ffc7f0e9497c9e3502ee016ad49fb1767a0a
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-06-22 01:39:25 -0400

    FIRST commit; practicing types

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //The Basics
+    // let firstValue: string = 'Manny';
+    // let firstValue: number = 34 ;
+    // let firstValue: boolean = true ;
+    // let firstValue: number[] = [2, 3, 56];
+    // let firstValue: Array<string> = ['2', '3', 'Manny'];
+
+  //A Tuple : a type of array where you can insert multiple types
+    // let aTuple: [string, number] = ['Manny', 34];
+
+  //An Enum: similar to objects with multiple items
+  // enum Codes (first = 1, second = 2 );
+  
+  //Any: use whenever you're not sure what they type is; use in prototypes
+  let firstName: any = '3';
+
+  //void: for voiding a return type
+  // const warning = () : void => {
+  //   console.log('Warning')
+  // }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          The values {firstName} is of {typeof firstName } type!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
 }
+
 
 export default App;
